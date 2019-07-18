51973e239610b4217bdaa81e13762b705b79508d
yuanshi <15251805961@163.com>
2019-07-18 21:22:58 +0800

    Initial commit

---
+++ src/main/java/com/macro/vsearch/VsearchApplication.java
@@ -0,0 +1,13 @@
+package com.macro.vsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VsearchApplication.class, args);
+    }
+
+}
