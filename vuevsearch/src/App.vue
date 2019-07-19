9f5355c712bfeeaa0126a1e1ad778931e6981f86
yuanshi <15251805961@163.com>
2019-07-19 10:24:45 +0800

    #comment ------------集成前端Vue-------------

---
+++ vuevsearch/src/App.vue
@@ -0,0 +1,43 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <div id="app">
+        {{msg}}
+        <div>
+            <input type="text" v-model="info">
+            <button v-on:click="handleClick">添加</button>
+        </div>
+        <ul>
+            <todo-item v-for="item in list" :key="item">
+              <template v-slot:item="itemProps">
+                <span :style="{fontSize: '20px', color: itemProps.checked ? 'red': 'blue'}">{{item}}</span>
+              </template>
+            </todo-item>
+        </ul>
+    </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+export default {
+  name: 'app',
+  data() {
+    return {
+        msg: 'hello world',
+        info: '',
+        list: [],
+    }
+  },
+  methods: {
+    handleClick() {
+        this.list.push(this.info)
+        this.info = ''
+    }
+  },
+  components: {
+    TodoItem,
+  }
+}
+</script>
+
+<style>
+
+</style>
