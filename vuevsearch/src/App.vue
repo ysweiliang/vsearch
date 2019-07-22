bb5d95848f2e55c42ef05b409b71416ab2d9acf6
yuanshi <15251805961@163.com>
2019-07-23 01:10:55 +0800

    ----------------------------------

---
+++ vuevsearch/src/App.vue
@@ -0,0 +1,33 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+    <div id="app">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import TodoItem from './components/TodoItem.vue'
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: 'hello world',
+                info: '',
+                list: [],
+            }
+        },
+        methods: {
+            handleClick() {
+                this.list.push(this.info)
+                this.info = ''
+            }
+        },
+        components: {
+            TodoItem,
+        }
+    }
+</script>
+
+<style>
+
+</style>
